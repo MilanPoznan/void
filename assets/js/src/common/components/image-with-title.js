99f998f6274b393f933905a47aa78fa21fef6ad3
Djordje Stojanovic <djordje@jl.is>
2019-07-22 15:04:49 +0200

    Style for image with title section

--- assets/js/src/common/components/image-with-title.js
+++ assets/js/src/common/components/image-with-title.js
@@ -5,15 +5,9 @@ export default function imageWithTitle(result) {
       ${result.acf.image_with_title_repeater.map(
         item => `
         <div class="image-text-component__wrapper">
-          <a href="${item.component_link}">
-            <div class="image-text-component__overlay"></div>
-            <div class="image-text-component__image" style="background: url('${item.component_image}')">
-            </div>
-            <div class="image-text-component__content">
-              <div class="image-text-component__content-title">
-              ${item.component_title}
-              </div>
-            </div>
+          <a href="${item.component_link}" class="image-text-component__link">
+          <img class="image-text-component__image" src="${item.component_image}">
+          <div class="image-text-component__overlay-title">${item.component_title}</div>
           </a>
         </div>`
       ).join('')}
