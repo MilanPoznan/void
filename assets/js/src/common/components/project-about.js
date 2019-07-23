1bed611f8a46e2af6722ff4d31048636434cdbb8
Djordje Stojanovic <djordje@jl.is>
2019-07-23 10:58:34 +0200

    Basic style for hero section

--- assets/js/src/common/components/project-about.js
+++ assets/js/src/common/components/project-about.js
@@ -2,13 +2,7 @@ export default function projectAbout(result) {
   return (
     `<div class="about-project" style="border: 1px solid red">
       <div class="about-project__wrapper">
-        <div class="about-project__short-info">
-        ${result.acf.project_details.map(item => `<div>${item.project_detail}</div>`).join('')}
-        ${result.acf.project_external_links.map(item =>
-          `<a target="_blank" href="${item.url}" style="background: url('${item.icon}')"></a>`
-        ).join('')}
 
-        </div>
         <div class="about-project__description">
           ${result.acf.about_project}
         </div>
--- assets/js/src/common/components/project-hero.js
+++ assets/js/src/common/components/project-hero.js
@@ -2,11 +2,20 @@ export default function projectHero(result) {
   return (
     `<div class="hero">
       <div class="hero__wrapper">
-        <div class="hero__content">
-          <div class="hero__title">${result.title.rendered}</div>
-          <div class="hero__title">${result.acf.project_description}</div>
+        <div class="hero__image" style="background-image: url('${result.acf.project_image}')"></div>
+        <div class="hero__content wrapper">
+          <div class="hero__titles">
+            <h3 class="hero__subtitle">${result.acf.project_description}</h3>
+            <h1 class="hero__title">${result.title.rendered}</h1>
+          </div>
+          <div class="hero__short-info">
+          ${result.acf.project_details.map(item => `<div>${item.project_detail}</div>`).join('')}
+          ${result.acf.project_external_links.map(item =>
+            `<a target="_blank" href="${item.url}" style="background: url('${item.icon}')"></a>`
+          ).join('')}
+  
+          </div>
         </div>
-        <div class="hero__image" style="background: url('${result.acf.project_image}')"> </div>
       </div>
     </div>`
   )
