6c6bdf550a56b3f18374f1421f79a071033d5602
Djordje Stojanovic <djordje@jl.is>
2019-07-24 19:52:59 +0200

    Add event listener

--- assets/js/src/common/components/project-ekipa.js
+++ assets/js/src/common/components/project-ekipa.js
@@ -1,11 +1,29 @@
 import sliderFunction from './slider';
+import { log } from 'util';
 
 export default function ekipaFilma(result) {
   const projectMainWrapper = document.querySelector('.single-project');
   let sliderIndex = 0;
   const body = document.getElementsByTagName('body');
-  sliderFunction(body[0], 'team__member-wrapp', 'team', sliderIndex);
+  sliderFunction(body[0], 'team__member', 'team', sliderIndex);
 
+  body[0].addEventListener('click', (e) => {
+    let isClicked = false;
+
+    if(e.target.parentNode.className === 'team__member-wrapp') {
+      if(!isClicked) {
+        e.target.parentNode.className += ' move-top';
+        isClicked = true;
+        console.log(isClicked);
+      } else {
+        e.target.parentNode.className -= ' move-top';
+        isClicked = false;
+        console.log(isClicked);
+      }
+    } else {
+      console.log(e.target.className)
+    }
+  });
   
   return (
     `<div class="team">
@@ -18,10 +36,10 @@ export default function ekipaFilma(result) {
           ${result.acf.ekipa_filma.map(item => 
             `<div class="team__member" style="transition: 0.5s;">
               <div class="team__member-wrapp">
-              <div class="team__member-image" style="background-image: url('${item.image}')"></div>
-              <h3 class="team__member-name">${item.name}</h3>
-              <div class="team__member-position">${item.position}</div>
-              <div class="team__member-description">${item.description}</div>
+                <div class="team__member-image" style="background-image: url('${item.image}')"></div>
+                <h3 class="team__member-name">${item.name}</h3>
+                <div class="team__member-position">${item.position}</div>
+                <div class="team__member-description">${item.description}</div>
               </div>
             </div>`  
           ).join('')}
