8f2e36165d32c441f434cf95f7834c89503b7a02
Djordje Stojanovic <djordje@jl.is>
2019-07-24 10:18:22 +0200

    Styling single project for mobile

--- assets/js/src/common/components/project-about.js
+++ assets/js/src/common/components/project-about.js
@@ -1,12 +1,9 @@
 export default function projectAbout(result) {
   return (
-    `<div class="about-project" style="border: 1px solid red">
-      <div class="about-project__wrapper">
-
-        <div class="about-project__description">
-          ${result.acf.about_project}
-        </div>
-      </div>
+    `<div class="about-project wrapper">
+      <div class="about-project__description">
+        ${result.acf.about_project.map(item => `<div class="about-project__box">${item.about_project_box}</div>`).join('')}
+      </div>  
     </div>`
   )
 
--- assets/js/src/common/components/project-ekipa.js
+++ assets/js/src/common/components/project-ekipa.js
@@ -8,23 +8,23 @@ export default function ekipaFilma(result) {
   return (
     `<div class="team">
       <div class="team__wrapper">
-      <div class="team__title">
-        <h3>Our crew</h3>
-        <h1>TEAM</h1>
-      </div>
-      <div class="team__peoples-slider">
-        ${result.acf.ekipa_filma.map(item => 
-          `<div class="team__member-wrapp" style="transition: 0.5s;">
-            <div class="team__member-image" style="background: url('${item.image}')"></div>
-            <h3>${item.name}</h3>
-            <div class="team__position">${item.description}</div>
-          </div>`  
-        ).join('')}
+        <div class="team__titles">
+          <h3>${result.acf.team_subtitle}</h3> 
+          <h1>${result.acf.team_title}</h1>
+        </div>
+        <div class="team__peoples-slider">
+          ${result.acf.ekipa_filma.map(item => 
+            `<div class="team__member-wrapp" style="transition: 0.5s;">
+              <div class="team__member-image" style="background-image: url('${item.image}')"></div>
+              <h3 class="team__member-name">${item.name}</h3>
+              <div class="team__member-position">${item.description}</div>
+            </div>`  
+          ).join('')}
         </div>
       </div>
       <span class="team__prev team__slide"></span>
       <span class="team__next team__slide"></span>
-
+      
     </div>`
   )
 }
