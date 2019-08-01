b4e521d7e9e15ed8acd9b55ab1a72e301e889950
Djordje Stojanovic <djordje@jl.is>
2019-08-01 14:16:26 +0200

    Project about desktop basic style

--- assets/js/src/common/components/project-about.js
+++ assets/js/src/common/components/project-about.js
@@ -2,7 +2,8 @@ export default function projectAbout(result) {
   return (
     `<div class="about-project wrapper">
       <div class="about-project__description">
-        ${result.acf.about_project.map(item => `<div class="about-project__box">${item.about_project_box}</div>`).join('')}
+        ${result.acf.about_project.map(item => `<div class="about-project__box">
+          ${item.about_project_box}</div>`).join('')}</div>
       </div>  
     </div>`
   )
