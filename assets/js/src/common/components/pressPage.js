ca79965c510b21832c5b1a2d996ee2448d875565
Milan Poznan <poznan.milan@gmail.com>
2019-08-24 14:55:20 +0200

    header and press are almost done

--- assets/js/src/common/components/pressPage.js
+++ assets/js/src/common/components/pressPage.js
@@ -9,9 +9,10 @@ export default function pressPage(result) {
   }
   
   body[0].onclick = (e) => {
-    console.log(e.target.className);
-    
     if (e.target.className.includes('press__top-arrow')) {
+      let test = $(e.target).closest('div').find('.press__bottom');
+      console.log(test);
+      
       $('.press__bottom').slideToggle('slow');
       $('.js-press-arrow').toggleClass('press__top-arrow--open');
       
@@ -24,21 +25,21 @@ export default function pressPage(result) {
         ${result.acf.press_repeater.map(
           item => `
             <div class="press__section">
-              <div class="press__top">
+              <div class="press__top press__section--top">
                 <div class="press__image" style="background-image: url('${item.section_image.url}')"></div>
-                <div class="press__title">${item.section_title}</div>
-                <div class="press__subtitle">${item.section_subtitle}</div>
+                <div class="press__title-wrapp">
+                  <h4 class="press__subtitle">${item.section_subtitle}</h4>
+                  <h1 class="press__title">${item.section_title}</h1>
+                </div>
                 <div class="press__arrow-wrap">
                   <div class="press__top-arrow press__arrow js-press-arrow js-show-arrow"></div>
                 </div>
               </div>
               <div class="press__bottom">
                 <div class="press__bottom-head">
-                  <div class="press__title">${item.section_title}</div>
-                  <div class="press__subtitle">${item.section_subtitle}</div>
                 </div>
                 <div class="press__content">${item.section_content}</div>
-                <div class="press__arrow-wrap" onClick="{$('.press__bottom').toggle('slow')}">
+                <div class="press__arrow-wrap">
                   <div class="press__bottom-arrow press__arrow js-press-arrow js-hide-arrow"></div>
                 </div>
 
