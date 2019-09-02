03341a35f3c1fd5bba14e9764aaed7d707b1654b
Milan Poznan <poznan.milan@gmail.com>
2019-09-02 15:45:12 +0200

    homepage links are fixed

--- assets/js/src/common/components/image-with-title.js
+++ assets/js/src/common/components/image-with-title.js
@@ -7,7 +7,6 @@ export default function imageWithTitle(result) {
         item => `
         <div class="image-text-component__wrapper">
           <a class="image-text-component__link" href="${item.component_link}"   style="background-image: url('${item.component_image}')">
-            <div class="image-text-component__overlay"></div>
             <div class="image-text-component__title">${item.component_title}</div>
           </a>
         </div>`
